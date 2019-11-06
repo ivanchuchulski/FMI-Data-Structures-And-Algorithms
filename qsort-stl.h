bc67dc9b93985e2ca43d14a731b01e8b47bc5f64
Ivan Chuchulski <zake6@abv.bg>
2019-11-06 19:02:04 +0200

    qsort stl

---
+++ qsort-stl.h
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+
+template <class ForwardIt>
+void quicksort(ForwardIt first, ForwardIt last)
+{
+	if (first == last) return;
+	auto pivot = *std::next(first, std::distance(first, last) / 2);
+
+	ForwardIt middle1 = std::partition(first, last, [pivot](const auto& em) { return em < pivot; });
+	ForwardIt middle2 = std::partition(middle1, last, [pivot](const auto& em) { return !(pivot < em); });
+
+	quicksort(first, middle1);
+	quicksort(middle2, last);
+}
